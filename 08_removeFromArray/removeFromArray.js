606ab2288264d1e99aba0518ccf2776f3105166d
Aiden Wei <240855557+aaiiddenwei@users.noreply.github.com>
2025-11-08 09:52:56 -0800

    Complete 06-11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    for (let el of remove) {
+        while (arr.includes(el)) {
+            arr.splice(arr.indexOf(el), 1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number" || a < 0 || b < 0 || a % 1 != 0 || b % 1 != 0) {
+        console.log(typeof a)
+        return "ERROR"
+    }
+    let sum = 0
+    const sortedArr = [a, b].sort((a, b) => a - b)
+    a = sortedArr[0]
+    b = sortedArr[1]
+    for (let i = a; i <= b; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
